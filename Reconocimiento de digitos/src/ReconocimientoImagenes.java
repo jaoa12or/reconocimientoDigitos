4c1f5320a318eda3fb7f2e10435b13fc8ec23f45
jaoa12or <jhon.orobio@correounivalle.edu.co>
2017-05-18 12:00:40 -0500

    Update ReconocimientoImagenes.java

--- Reconocimiento de digitos/src/ReconocimientoImagenes.java
+++ Reconocimiento de digitos/src/ReconocimientoImagenes.java
@@ -1,5 +1,6 @@
 
 
+
 import java.io.*;
 import java.util.StringTokenizer;
 import java.awt.Color;
@@ -9,6 +10,7 @@ import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
 import Jama.Matrix;
+import Jama.SingularValueDecomposition;
 
 //"C:/Users/user/Desktop/numeros/test/0/"+numi+".png"
 
@@ -30,6 +32,7 @@ public class ReconocimientoImagenes {
 	private Matrix matriz7;
 	private Matrix matriz8;
 	private Matrix matriz9;
+	private double[] valores;
 	
 	
 	public ReconocimientoImagenes()
@@ -45,19 +48,22 @@ public class ReconocimientoImagenes {
 			matriz7= new Matrix(784, 300);
 			matriz8= new Matrix(784, 300);
 			matriz9= new Matrix(784, 300);
+			valores = new double[10];
 			
 	}
 	
 	
 	
-	public void llenarMatrices( int arch)
+	
+	public Matrix llenarMatriz( int arch)
 	{
 		  String cadena;
 		  FileReader f;
+		  Matrix matriz = new Matrix(784, 300);
 		try {
 			
 			
-			f = new FileReader("C:\\Users\\user\\Desktop\\archivos proyecto\\"+arch+".txt");
+			f = new FileReader("/home/invitado/"+arch+".txt");
 			BufferedReader b = new BufferedReader(f);
 			cadena = b.readLine();
 			StringTokenizer token = new StringTokenizer(cadena);
@@ -72,23 +78,69 @@ public class ReconocimientoImagenes {
 			        		  
 			        		      
 			        		  datoString = token.nextToken();
-			        		  matriz0.set(w,r, Double.parseDouble(datoString));
-			        		  System.out.print( matriz0.get(w, r));
+			        		  matriz.set(w,r, Double.parseDouble(datoString));
+			        		  
+			        		  //System.out.print( matriz0.get(w, r));
 			    		      
 			        	  }
 			        	  
-			        	  System.out.println();
+			        	  //System.out.println();
 			          }
 		          
 		       
-		      
+		     
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		 return matriz;
 	      
 	}
+	
+	
+	
+	public void llenarMatrices()
+	{
+		matriz0= llenarMatriz( 0);
+		matriz1= llenarMatriz( 1);
+		matriz2= llenarMatriz( 2);
+		matriz3= llenarMatriz( 3);
+		matriz4= llenarMatriz( 4);
+		matriz5= llenarMatriz( 5);
+		matriz6= llenarMatriz( 6);
+		matriz7= llenarMatriz( 7);
+		matriz8= llenarMatriz( 8);
+		matriz9= llenarMatriz( 9);
+	}
+	
+	public Matrix svd(Matrix matriz)
+	{
+		SingularValueDecomposition svd = matriz.svd();
+		return svd.getU();
+		
+		
+	}
+	
+	public void comparacion()
+	{
+		Matrix identidad = new Matrix(784,784).identity(784, 784);
+		Matrix arreglo = new Matrix(imagenM);
+		valores[0] = identidad.minus(svd(matriz0).times(svd(matriz0).transpose())).times(arreglo).norm2();
+		valores[1] = identidad.minus(svd(matriz1).times(svd(matriz1).transpose())).times(arreglo).norm2();
+		valores[2] = identidad.minus(svd(matriz2).times(svd(matriz2).transpose())).times(arreglo).norm2();
+		valores[3] = identidad.minus(svd(matriz3).times(svd(matriz3).transpose())).times(arreglo).norm2();
+		valores[4] = identidad.minus(svd(matriz4).times(svd(matriz4).transpose())).times(arreglo).norm2();
+		valores[5] = identidad.minus(svd(matriz5).times(svd(matriz5).transpose())).times(arreglo).norm2();
+		valores[6] = identidad.minus(svd(matriz6).times(svd(matriz6).transpose())).times(arreglo).norm2();
+		valores[7] = identidad.minus(svd(matriz7).times(svd(matriz7).transpose())).times(arreglo).norm2();
+		valores[8] = identidad.minus(svd(matriz8).times(svd(matriz8).transpose())).times(arreglo).norm2();
+		valores[9] = identidad.minus(svd(matriz9).times(svd(matriz9).transpose())).times(arreglo).norm2();
+		
+	}
+	
+	
 
 	public void convertirImagen(String ruta)
 	{
@@ -127,7 +179,7 @@ public class ReconocimientoImagenes {
 					count++;
 				}
 				
-				System.out.println();
+				
 			}
 			
 		} catch (IOException e) {
@@ -138,19 +190,19 @@ public class ReconocimientoImagenes {
 	}
 	
 		
-	public void matrizDeimagen(int numi)
+	public void matrizDeimagen(int numi,int numc)
 	{
 		
 		
 		try {
 			
 			InputStream archivo;
-			archivo = new FileInputStream("C:/Users/user/Desktop/numeros/test/0/"+numi+".png");
+			archivo = new FileInputStream("/home/invitado/test2/"+numc+"/"+numi+".png");
 			ImageInputStream imagen = ImageIO.createImageInputStream(archivo);
 			BufferedImage imagenB = ImageIO.read(imagen);
 			
 		
-			
+		
 			for(int w = 0; w< 28;w++)
 			{
 				for (int r = 0; r<28;r++)
@@ -184,6 +236,16 @@ public class ReconocimientoImagenes {
 		
 	}
 	
+	public double[] getValores() {
+		return valores;
+	}
+
+
+
+
+	public void setValores(double[] valores) {
+		this.valores = valores;
+	}
 	
 	
 	public void llenarBase(int numeroDeimagenes )
@@ -197,7 +259,7 @@ public class ReconocimientoImagenes {
 		for(int j = 0 ; j<10; j++)
 			{ count = 0;
 				
-				f = new File("C:\\Users\\user\\Desktop\\archivos proyecto\\"+j+".txt");
+				f = new File("/home/invitado/"+j+".txt");
 				fw = new FileWriter(f);
 				bw = new BufferedWriter(fw);
 				pw = new PrintWriter(bw);
@@ -206,7 +268,7 @@ public class ReconocimientoImagenes {
 				
 				for (int i = 1; i<= numeroDeimagenes;i++ )
 				{
-					matrizDeimagen(i);
+					matrizDeimagen(i,j);
 					
 					
 				}
@@ -232,9 +294,12 @@ public class ReconocimientoImagenes {
 
 		}
 		
+		
+		
+		
 	
 		
-		
+		//C:/Users/user/Desktop/numeros/test
 		
 	}
 	
@@ -247,9 +312,18 @@ public class ReconocimientoImagenes {
 		
 		ReconocimientoImagenes obj = new ReconocimientoImagenes();
 		
-		obj.convertirImagen("C:/Users/user/Desktop/numeros/test/0/1.png");
-		obj.llenarBase(300);
-		obj.llenarMatrices(0);
+		//obj.llenarBase(300);
+		obj.llenarMatrices();
+		obj.convertirImagen("/home/invitado/test2/5/510.png");
+		obj.comparacion();
+		
+		for(int i = 0; i<10;i++)
+		{
+			System.out.println(i+" "+(obj.getValores())[i]);
+		}
+		
+		
+		
 		
 		
 		// TODO Auto-generated method stub
